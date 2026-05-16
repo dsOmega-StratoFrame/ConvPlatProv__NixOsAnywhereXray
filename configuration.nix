e047c163ca99df9d068ee7b7067346b2f087f966
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-05-16 17:12:02 +0300

    fix(configuration): enabled lvm for diskio to work

--- configuration.nix
+++ configuration.nix
@@ -16,11 +16,14 @@ in
     allowUnfree = true;
   };
 
-  boot.loader.grub = {
-    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-    # devices = [ ];
-    efiSupport = true;
-    efiInstallAsRemovable = true;
+  boot = {
+    initrd.lvm.enable = true;
+    loader.grub = {
+      # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+      # devices = [ ];
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+    };
   };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
